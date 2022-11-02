9c3bcf951b6ba5f9d52b061d1cb30591e3e634a3
Александр <7176uiop3436>
2022-11-02 17:43:51 +0300

    Созданы функции для управления бд

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -2,35 +2,26 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
-
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
-
-
-# def get_user_by_email(db: Session, email: str):
-#     return db.query(models.User).filter(models.User.email == email).first()
-
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db_user = models.User(phone=user.phone,
+                          login=user.login,
+                          password=user.password,
+                          name=user.name,
+                          birth=user.birth,
+                          tg=user.tg,
+                          email=user.email
+                          )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+def autenticate_user(db: Session, user_login: str, user_password: str):
+    return db.query(models.User).filter(models.User.login == user_login and models.User.password == user_password)
 
-# def get_items(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Item).offset(skip).limit(limit).all()
-#
-#
-# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-#     db_item = models.Item(**item.dict(), owner_id=user_id)
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
