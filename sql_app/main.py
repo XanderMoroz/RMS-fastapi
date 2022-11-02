700e9eb7f2b14b378ccd46148231451af2427aa6
Александр <7176uiop3436>
2022-11-02 17:46:26 +0300

    Подключена БД и сессии, созданы урлы, сформулирована логика

--- sql_app/main.py
+++ sql_app/main.py
@@ -18,36 +18,23 @@ def get_db():
         db.close()
 
 
-@app.post("/v1/auth/register", response_model=schemas.User)
+@app.post("/v1/auth/register", response_model=schemas.UserID)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
+@app.post("/v1/auth/login", response_model=schemas.UserID)
+def auth_user(user_login: str, user_password: str, db: Session = Depends(get_db)):
+    db_user = crud.autenticate_user(db, user_login=user_login, user_password=user_password)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Login and password are not correct.")
+    return db_user
 
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/v1/user/{user_id}", response_model=schemas.User)
+@app.get("/v1/user/{user_id}", response_model=schemas.UserResponse)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
-
-
-# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
-# def create_item_for_user(
-#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-# ):
-#     return crud.create_user_item(db=db, item=item, user_id=user_id)
-#
-#
-# @app.get("/items/", response_model=list[schemas.Item])
-# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     items = crud.get_items(db, skip=skip, limit=limit)
-#     return items
