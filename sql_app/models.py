50a0488529aa465f4cc999ed3c5f5c7034e3526a
Александр <7176uiop3436>
2022-11-02 17:44:43 +0300

    Создана модель User

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,26 +1,15 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Date
 
 from .database import Base
-
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
-
-
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
+    phone = Column(String, comment='Номер телефона пользователя')
+    login = Column(String(128), comment='Логин')
+    password = Column(String(128), comment='Пароль')
+    name = Column(String(128), comment='Имя пользователя')
+    birth = Column(Date, comment='Дата рождения пользователя')
+    tg = Column(String(128), nullable=True, comment='Телеграмм пользователя')  # необязательное поле
+    email = Column(String(128), nullable=True, comment='Email пользователя')  # необязательное поле
